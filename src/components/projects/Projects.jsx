8041f4109d9a42313f3fe5d36888f100a578870b
emeraldknytt <emeraldknytt@gmail.com>
2024-08-07 01:54:41 +0100

    Changes(): Few modifications to optimize webview

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,35 +1,33 @@
 import { projectData } from "../../../data";
 import Button from "../../layout/button/Button";
 import ProjectCard from "../projectCard/ProjectCard";
-import classes from './Projects.module.css'
-
+import classes from "./Projects.module.css";
 
 const Projects = () => {
-    return (
-        <section className={classes.project_container}>
+  return (
+    <section className={classes.project_container}>
+      <div className={classes.project_title}>
+        <h2>Projects</h2>
+        <Button text={"CONTACT ME"} />
+      </div>
 
-            <div className={classes.project_title}>
-                <h2>Projects</h2>
-                <Button text={'CONTACT ME'}/>
-            </div>
-            
-
-            <div className={classes.project_cards}>
-                {projectData.map((item, i) => {
-                    return (
-                        <ProjectCard 
-                        img={item.img} 
-                        title={item.title} 
-                        tagOne={item.tagOne} 
-                        tagTwo={item.tagTwo} 
-                        tagThree={item.tagThree} 
-                        key={i}
-                        />
-                    );
-                })}
-            </div>
-        </section>
-    )
-}
+      <div className={classes.project_cards}>
+        {projectData.map((item, i) => {
+          return (
+            <ProjectCard
+              img={item.img}
+              title={item.title}
+              tagOne={item.tagOne}
+              tagTwo={item.tagTwo}
+              tagThree={item.tagThree}
+              key={i}
+            />
+          );
+        })}
+      </div>
+    </section>
+  );
+};
 
 export default Projects;
+ 
